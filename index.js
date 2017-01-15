2a08bcfe287981f647842cc5e41a3448de84f1ae
Myles <myleschaynes@gmail.com>
2017-01-14 18:08:32 -0800

    I'm going to kill myself

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ var fs = require('fs-extra');
 var bodyParser = require('body-parser');
 var http = require('http');
 var exec = require('child_process').exec;
-
+//Connects to Oxford API
+var oxford = require('project-oxford');
+client = new oxford.Client('f4d97dc46d7644f8ab6c401711ac5287'); //String is API Key
 var app = express();
 
+var faceListID = 'list';
+var personGroup = 'person_group'
 app.use(bodyParser());
 
 app.get('/api/whowas', function(req, res) {
@@ -26,9 +30,16 @@ app.post('/api/whenwas', function(req, res) {
 });
 
 app.post('/api/add',function(req, res) {
-  takePicture((result) => {
-    res.send(result);
-  });
+  var image_path;
+  //LOOK AT THIS IAN. MAKE SURE YOU FUCKING REQUEST CORRECTLY.
+  var name = req.body.name;
+  function addPerson(image_path) {
+    client.face.person.create(personGroup, name, Date.now()).then(response => {
+      client.face.person.addFace(groupID, response.personId, {path:image_path}).then(addedFace => {
+      });
+    });
+  }
+  takePicture(addPerson);
 });
 
 function takePicture(callback) {
--- test.js
+++ test.js
@@ -1,21 +1,49 @@
 /**
  * @description Server for the application; Uses express
  ******************************************************************************/
-var app = require('express');
-var fs = require('fs');
+
+var express = require('express');
+var fs = require('fs-extra');
+var bodyParser = require('body-parser');
+var http = require('http');
+//Connects to Oxford API
 var oxford = require('project-oxford');
-client = new oxford.Client('f4d97dc46d7644f8ab6c401711ac5287');
+client = new oxford.Client('f4d97dc46d7644f8ab6c401711ac5287'); //String is API Key
+
+var app = express();
+var faceListID = 'list';
+var personGroup = 'person_group'
+app.use(bodyParser());
+
+app.get('/api/whowas', function(req, res) {
+
+});
+
+app.get('/api/whois', function(req, res) {
+});
+
+app.post('/api/whenwas', function(req, res) {
+
+});
+
+app.post('/api/add',function(req, res) {
+  var image_path;
+  //LOOK AT THIS IAN. MAKE SURE YOU FUCKING REQUEST CORRECTLY.
+  var name = req.body.name;
+  function add_person(image_path) {
+    client.face.person.create(personGroup, name, Date.now()).then(response => {
+      client.face.person.addFace(groupID, response.personId, {path:image_path}).then(addedFace => {
+      });
+    });
+  }
+  var exec = require('child_process').exec;
+  exec('java -cp ./src/java/CamWork', {cwd: './'}, function(err, stdout, stderr) {
+    var image_path = stdout;
+  });
 
 
-function createPerson() {
-
-}
-
-function print_everyone() {
-
-}
+});
 
 
-function getPeople() {
 
-}
+app.listen(process.env.PORT || 8000);
